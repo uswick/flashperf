c8685f420f55615b689317b4ff5de086b16b79a1
uswick <mastershield2007@gmail.com>
2019-02-07 02:03:19 -0500

    add initial test case for fperf

---
+++ testperf.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "perf.h"
+
+PERF_DECL();
+
+int main(int argc, char **argv) {
+
+  //PERF_START(INS_COUNT);
+  PERF_START(L1_D_MISSES);
+  printf("test print\n");
+  //PERF_END(INS_COUNT);
+  PERF_END(L1_D_MISSES);
+  return 0;
+}
+
