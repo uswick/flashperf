2a18dcb31234cbdaf4962afe67973ca0710ab848
Udayanga Wickramasinghe <uswickra@umail.iu.edu>
2019-02-24 02:43:07 -0500

    add test code

--- testperf.c
+++ testperf.c
@@ -8,9 +8,11 @@ int main(int argc, char **argv) {
 
   //PERF_START(INS_COUNT);
   PERF_START(L1_D_MISSES);
+  //PERF_START(NB_CB_MREQ1);
   printf("test print\n");
   //PERF_END(INS_COUNT);
   PERF_END(L1_D_MISSES);
+  //PERF_END(NB_CB_MREQ1);
   return 0;
 }
 
